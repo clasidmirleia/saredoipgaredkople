2063a757870dcfd545f81c448b533ca6fe173353
Thegcle Focean <parmeshdinda@outlook.com>
2025-12-20 12:35:09 +0530

    Update two.sh.

--- two.sh
+++ two.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Clone target repo
-git clone https://fredsuiopaweszxkguqopzx-admin@bitbucket.org/fredsuiopaweszxkguqopzxes/us-ac-v1-007-of-one.git /tmp/repo
+git clone https://fredsuiopaweszxkguqopzx-admin@bitbucket.org/fredsuiopaweszxkguqopzxes/us-ac-v1-007-of-two.git /tmp/repo
 cd /tmp/repo
 
 # Extract base image from Dockerfile
